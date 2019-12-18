8342cb70a74804bac04dcfe5aaa77c6fb36fd36f
aaapoiu <hyo5552@gmail.com>
2019-12-18 16:49:18 +0900

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World hohohoho"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
